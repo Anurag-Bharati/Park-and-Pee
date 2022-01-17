fb3a894df598a3dd0b2de94319d6f5f29e8b4f70
Rohankhadka33 <rohankhadka33@gmail.com>
2022-01-17 19:32:59 +0545

    made some changes in admin dashboard

--- lib/admin.dart
+++ lib/admin.dart
@@ -11,10 +11,95 @@ class _MyadminState extends State<Myadmin> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage('assets/admin.png'), fit: BoxFit.fill),
-      ),
-    );
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/admin3.png'), fit: BoxFit.fill),
+        ),
+        child: SafeArea(
+          top: true,
+          child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: Colors.transparent,
+            body: Stack(
+              children: [
+                SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 20),
+                          child: Form(
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Column(
+                                            // ignore: prefer_const_literals_to_create_immutables
+                                            children: [
+                                              const Padding(
+                                                padding: EdgeInsets.only(
+                                                    left: 5, top: 10),
+                                                child: SizedBox(
+                                                  child: Text(
+                                                    "17th January, 2022",
+                                                    style: TextStyle(
+                                                        fontSize: 15,
+                                                        color: Colors.white),
+                                                  ),
+                                                ),
+                                              ),
+                                              const Padding(
+                                                padding: EdgeInsets.only(
+                                                    left: 15, top: 10),
+                                                child: SizedBox(
+                                                  child: Text(
+                                                    "Hello, Admin",
+                                                    style: TextStyle(
+                                                        fontSize: 25,
+                                                        color: Colors.white),
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                          Column(
+                                            children: [
+
+
+                                            ],
+
+
+                                          )
+                                        ],
+                                      ),
+                                      Row(
+
+                                        children: [
+                                          
+
+                                          
+                                        ],
+
+
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
