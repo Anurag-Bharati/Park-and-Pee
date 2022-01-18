3aa7fc2df6e07a66ca690ef462aeb2d6e99ce84c
Rohankhadka33 <rohankhadka33@gmail.com>
2022-01-18 16:06:48 +0545

    Added admin photo

--- lib/admin.dart
+++ lib/admin.dart
@@ -27,7 +27,7 @@ class _MyadminState extends State<Myadmin> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Container(
-                          padding: EdgeInsets.symmetric(
+                          padding: const EdgeInsets.symmetric(
                               horizontal: 10, vertical: 20),
                           child: Form(
                             child: Column(
@@ -35,57 +35,73 @@ class _MyadminState extends State<Myadmin> {
                                 Container(
                                   child: Column(
                                     children: [
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Column(
-                                            // ignore: prefer_const_literals_to_create_immutables
-                                            children: [
-                                              const Padding(
-                                                padding: EdgeInsets.only(
-                                                    left: 5, top: 10),
-                                                child: SizedBox(
-                                                  child: Text(
-                                                    "17th January, 2022",
-                                                    style: TextStyle(
-                                                        fontSize: 15,
-                                                        color: Colors.white),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 0, left: 5, right: 5),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Column(
+                                              // ignore: prefer_const_literals_to_create_immutables
+                                              children: [
+                                                const Padding(
+                                                  padding: EdgeInsets.only(
+                                                      left: 5, top: 10),
+                                                  child: SizedBox(
+                                                    child: Text(
+                                                      "17th January, 2022",
+                                                      style: TextStyle(
+                                                          fontSize: 15,
+                                                          color: Colors.white),
+                                                    ),
                                                   ),
                                                 ),
+                                                const Padding(
+                                                  padding: EdgeInsets.only(
+                                                      left: 15, top: 10),
+                                                  child: SizedBox(
+                                                    child: Text(
+                                                      "Hello, Admin",
+                                                      style: TextStyle(
+                                                          fontSize: 25,
+                                                          color: Colors.white),
+                                                    ),
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 0, right: 25),
+                                              child: Column(
+                                                children: const [
+                                                  CircleAvatar(
+                                                    backgroundImage: AssetImage(
+                                                        'assets/anurag.jpg'),
+                                                    radius: 35,
+                                                  )
+                                                ],
                                               ),
-                                              const Padding(
-                                                padding: EdgeInsets.only(
-                                                    left: 15, top: 10),
-                                                child: SizedBox(
-                                                  child: Text(
-                                                    "Hello, Admin",
-                                                    style: TextStyle(
-                                                        fontSize: 25,
-                                                        color: Colors.white),
-                                                  ),
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                          Column(
-                                            children: [
-
-
-                                            ],
-
-
-                                          )
-                                        ],
+                                            )
+                                          ],
+                                        ),
                                       ),
                                       Row(
-
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                         children: [
-                                          
+                                          Column(
+                                            
 
-                                          
+                                            
+
+
+                                            
+                                            
+                                          )
+
+
                                         ],
-
 
                                       )
                                     ],
