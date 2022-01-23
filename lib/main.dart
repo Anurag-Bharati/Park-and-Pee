36d0785fe33f141f4d283b3d4157072b8a21d66b
Mohan Basnet <mbasnet201819@gmail.com>
2022-01-23 12:33:10 +0545

    configure dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ void main() {
     // home: const MyLogin(),
     // home: const MyVerification()
     // home: const Myadmin(),
-    home: const MySoNavbar(),
+    home: const Mynavbar(),
     routes: {
       'register': (context) => const MyRegister(),
       'login': (context) => const MyLogin(),
