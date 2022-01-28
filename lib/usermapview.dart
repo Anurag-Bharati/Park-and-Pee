fe80f840cc6624961690ae99dd0763ffc1d05984
Rohankhadka33 <rohankhadka33@gmail.com>
2022-01-28 12:03:47 +0545

    m

--- lib/usermapview.dart
+++ lib/usermapview.dart
@@ -1,6 +1,13 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:parkandpee/Aboutus.dart';
+import 'package:parkandpee/Model/ScrollBehavior.dart';
+
+import 'Model/map_util.dart';
+import 'Model/progress_step_widget.dart';
 
 class UserMapview extends StatefulWidget {
   const UserMapview({Key? key}) : super(key: key);
@@ -40,6 +47,15 @@ class _UserMapviewState extends State<UserMapview> {
     return _currentLatLng;
   }
 
+  double? size() {
+    print("${MediaQuery.of(context).size}}");
+    if (deviceHeight(context) > 1000 && deviceWidth(context) > 500) {
+      return 800;
+    } else {
+      return 520;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,6 +65,43 @@ class _UserMapviewState extends State<UserMapview> {
         child: AppBar(
           elevation: 0,
           backgroundColor: Colors.transparent,
+        ),
+      ),
+
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(color: Colors.transparent),
+          child: ScrollConfiguration(
+            behavior: MyBehavior(),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: size(),
+                        child: GoogleMap(
+                          initialCameraPosition:
+                              CameraPosition(target: _currentLatLng, zoom: 15),
+                        ),
+                      ),
+                      Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                            // borderRadius: Radius.circular(radius)),
+                            ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
       // body: const SafeArea(
